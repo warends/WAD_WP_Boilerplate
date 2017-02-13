963cc43af8958a5f846ffd2359b1b6e437bfb4d4
Will Arends <willarends@gmail.com>
2017-02-12 18:30:24 -0800

    add enque scripts bootstrap and jquery

--- wp-content/themes/will-arends-development/functions.php
+++ wp-content/themes/will-arends-development/functions.php
@@ -111,6 +111,10 @@ function will_arends_development_scripts() {
 
 	wp_enqueue_script( 'will-arends-development-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
+	wp_enqueue_script( 'will-arends-development-bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '20151215', true );
+
+	wp_enqueue_script( 'will-arends-development-jquery', get_template_directory_uri() . '/js/jquery.min.js', array(), '20151215', true );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
