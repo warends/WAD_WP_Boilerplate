0e53b32c1db0cda6175585f2561bc68c89b83cd7
Will Arends <willarends@gmail.com>
2017-04-04 17:38:33 -0700

    bootstrap header

--- wp-content/themes/will-arends-development/header.php
+++ wp-content/themes/will-arends-development/header.php
@@ -15,6 +15,9 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
+<!-- Bootstrap -->
+<link href="<?php bloginfo('stylesheet_directory'); ?>/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
 <?php wp_head(); ?>
 </head>
@@ -24,26 +27,35 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'will-arends-development' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+		<div class="navbar-wrapper">
+		    <nav class="navbar navbar-default" role="navigation">
+		        <div class="container">
+		          <div class="navbar-header">
+		          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+		            <span class="sr-only">Toggle navigation</span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+		          </button>
+							<a class="navbar-brand" href="/">
+								<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/logo.png" alt="Will Arends Development"></a>
+		        </div><!--end navbar-header-->
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
+		          <?php
+		        wp_nav_menu(array (
+								'theme_location' => 'menu-1',
+								'menu_id' => 'primary-menu',
+		            'depth'           =>   2,
+		            'container'       =>  'nav',
+		            'container_class' =>  'navbar-collapse collapse',
+		            'container_id'    =>  'bs-example-navbar-collapse-1',
+		            'menu_class'      =>  'nav navbar-right navbar-nav'
+							));
+		      ?>
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'will-arends-development' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
+		      </div><!--/.container-->
+		    </nav>
+		</div><!--navbar-header-->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
